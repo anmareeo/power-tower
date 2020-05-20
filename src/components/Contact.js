42f23ffddd0a292303c61336a881eb687818a74c
Anmaree <anmaree.osmond@gmail.com>
2020-05-19 23:56:54 -0600

    contact form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -6,3 +6,33 @@ export default function Contact(){
     )
 }
 
+
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14//deciding how to create the form
+15
+class Form extends React.Component {
+	render() {
+  	return (
+    	<form>
+      <span className="formtext">&#x3C;Form /&#x3E;</span>
+    	  <input 
+          type="text" 
+          placeholder="Enter Company Name" 
+          required 
+        />
+        <button>Go!</button>
+    	</form>
+    );
+  }
